67664de55e9eced067c06cd95c93861921ce1417
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-01-19 18:16:37 +0200

    add create-image

---
+++ src/js/create-image.js
@@ -0,0 +1,36 @@
+function createListsImages(data, gallery) {
+  const newGallery = data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`;
+      }
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', newGallery);
+}
+
+export { createListsImages };
